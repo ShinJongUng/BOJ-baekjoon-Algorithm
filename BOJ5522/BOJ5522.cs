d9558d0b88de24c3fb0e8b7ad5a6bdecb7c00188
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-08-07 22:39:11 +0900

    Add files via upload

---
+++ BOJ5522/BOJ5522.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boj5522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+                Console.Write(a + b + c + d + e);
+            
+        }
+    }
+}
---
+++ BOJ5522/baekjoon BOJ5522 comment.md
@@ -0,0 +1,7 @@
+>Baejoon 5522
+
+```Bronze V / Math / Class null```
+
+[BOJ5522](https://www.acmicpc.net/problem/5522)<br>
+<h2> Algorithm -> A+B+C+D+E {<br>
+
