ae4fa469b7e12782902d550ed1b4b83a2f32e0b9
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-08-07 22:25:02 +0900

    Add files via upload

---
+++ BOJ6749/BOJ6749.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boj2338
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write((b - a) + b);
+        }
+    }
+}
---
+++ BOJ6749/baekjoon BOJ6749 comment.md
@@ -0,0 +1,7 @@
+>Baejoon 6749
+
+```Bronze V / Math / Class null```
+
+[BOJ6749](https://www.acmicpc.net/problem/6749)<br>
+<h2> Algorithm ->(M - Y) + M {<br>
+
