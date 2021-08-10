e5b8d478f69c325e93e5744fdef2e06045e29dd0
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-08-10 19:18:45 +0900

    Add files via upload

---
+++ BOJ15894/BOJ15727.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Numerics;
+
+
+namespace boj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long j = long.Parse(Console.ReadLine());
+
+            long result = j * 4;
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
