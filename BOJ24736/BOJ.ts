629ac9ca51dd96688d847713b7b35c6246ae1b77
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-05-02 17:20:57 +0900

    #2

--- BOJ24736/BOJ.ts
+++ BOJ24736/BOJ.ts
@@ -5,4 +5,5 @@ let f_output = f_input[0] * 6 + f_input[1] * 3 + f_input[2] * 2 + f_input[3] + f
 let s_input = input[1].split(" ").map((e)=>Number(e))
 let s_output = s_input[0] * 6 + s_input[1] * 3 + s_input[2] * 2 + s_input[3] + s_input[4] * 2
 
-console.log(f_output + " " + s_output)
+console.log(f_output)
+console.log(s_output)
