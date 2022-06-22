ec29537acf43d26f26a854cbb3cf213b0e02ac8b
[ShinJongUng] <whddnd0728@gmail.com>
2022-06-22 20:43:03 +0900

    #1

--- BOJ2743/BOJ.js
+++ BOJ2743/BOJ.js
@@ -1,3 +1,12 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './ans.txt';
-let input = require('fs').readFileSync(filePath).toString().trim().split("\n");
-console.log(input.join('').length)
+let input = require('fs').readFileSync(filePath).toString().trim().split("\n").join('');
+let s =""
+for(let i = 0; i < input.length; i++){
+    if(input[i] == input[i].toUpperCase()){
+        s += input[i].toLowerCase()
+    }
+    else{
+        s+= input[i].toUpperCase()
+    }
+}
+console.log(s)
