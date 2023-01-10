70369db8b5277d956572a288bfa24e2d0709314d
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2023-01-10 16:21:34 +0900

    #1

--- BOJ10828/BOJ.js
+++ BOJ10828/BOJ.js
@@ -1,65 +1,55 @@
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'ans.txt';
-let [N, ...inputs] = require('fs').readFileSync(filePath).toString().trim().split("\n");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ans.txt";
+let [N, ...inputs] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-class Queue{
-    constructor(arr){
-        this.arr = arr;
+class Stack {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  s_push(num) {
+    this.arr.push(num);
+  }
+  s_pop() {
+    let data = this.arr.pop();
+    if (!data) return -1;
+    return data;
+  }
+  s_empty() {
+    if (this.arr.length > 0) {
+      return 0;
+    } else {
+      return 1;
     }
-    s_push(num){
-        this.arr.push(num);
-    }
-    s_pop(){
-        let data = this.arr.shift();
-        if(!data) return -1
-        return data;
-    }
-    s_empty(){
-        if(this.arr.length > 0){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
-    s_size(){
-        return this.arr.length;
-    }
-    s_front(){
-        let data = this.arr[0];
-        if(!data) return -1;
-        else return data;
-    }
-    s_back(){
-        let data = this.arr[this.arr.length - 1];
-        if(!data) return -1;
-        else return data;
-    }
+  }
+  s_top() {
+    if (!this.arr[this.arr.length - 1]) return -1;
+    return this.arr[this.arr.length - 1];
+  }
+  s_size() {
+    return this.arr.length;
+  }
 }
 
+const arr = [],
+  ans = [];
+let stack = new Stack(arr);
+inputs.map((value) => {
+  value = value.trim().split(" ");
 
-const arr = [], ans = [];
-let queue = new Queue(arr);
-inputs.map((value)=>{
-    value = value.trim().split(" ");
+  if (value[0] == "push") {
+    stack.s_push(parseInt(value[1]));
+  } else if (value[0] == "top") {
+    ans.push(stack.s_top());
+  } else if (value[0] == "size") {
+    ans.push(stack.s_size());
+  } else if (value[0] == "pop") {
+    ans.push(stack.s_pop());
+  } else if (value[0] == "empty") {
+    ans.push(stack.s_empty());
+  }
+});
 
-    if(value[0] == "push"){
-        queue.s_push(parseInt(value[1]));
-    }
-    else if(value[0] == "empty"){
-        ans.push((queue.s_empty()));
-    }
-    else if(value[0] == "size"){
-        ans.push(queue.s_size());
-    }
-    else if(value[0] == "pop"){
-       ans.push(queue.s_pop());
-    }
-    else if(value[0] == "front"){
-        ans.push(queue.s_front());
-    }
-    else if(value[0] == "back"){
-        ans.push(queue.s_back());
-    }
-})
-
-console.log(ans.join("\n"))
+console.log(ans.join("\n"));
---
+++ BOJ2935/BOJ.py
@@ -0,0 +1,8 @@
+A = int(input())
+operator = str(input())
+B = int(input())
+
+if operator == "*":
+    print(A * B)
+else:
+    print(A + B)
