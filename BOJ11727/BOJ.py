e4b3fdfa49e4b4b7c2639ae9ac64588a1cf0ebc7
ShinJongUng <whddnd0728@gmail.com>
2023-01-18 20:12:59 +0900

    #1

--- BOJ11727/BOJ.py
+++ BOJ11727/BOJ.py
@@ -2,14 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-ans = [0, 1]
+ans = [1, 1]
 
 if n >= 2:
     for i in range(2, n + 1):
-        if i % 2 == 0:
-            ans.append(ans[i - 1] * 2 + 1)
-        else:
-            ans.append(ans[i - 1] * 2 - 1)
+        ans.append(ans[i - 1] + ans[i - 2])
     print(ans[n] % 10007)
 else:
     print(1)
