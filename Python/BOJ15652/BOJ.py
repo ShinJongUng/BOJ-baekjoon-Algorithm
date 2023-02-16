d85030cae86a7e52cbf75756a3cfab94b9927c18
ShinJongUng <whddnd0728@gmail.com>
2023-02-17 00:13:19 +0900

    Silver III: 조합(#3)

---
+++ Python/BOJ15652/BOJ.py
@@ -0,0 +1,8 @@
+import sys
+from itertools import combinations_with_replacement
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+for i in combinations_with_replacement(range(1, n + 1), m):
+    print(*i)
