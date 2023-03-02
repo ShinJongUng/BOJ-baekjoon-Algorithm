b836edbce199c522442c0ad97ba9b7b395e3b535
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2023-03-02 20:42:35 +0900

    [Gold IV] Title: 연구소, Time: 632 ms, Memory: 121944 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,15 +1,26 @@
-import sys
 from collections import deque
-import copy
-input = sys.stdin.readline
 
-# 4방향 탐색을 위한 dx, dy
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-# bfs 함수 정의
-def bfs(graph, x, y):
-    queue = deque([(x, y)])
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+virus = []
+walls = []
+safe_area = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            walls.append((i, j))
+            safe_area += 1
+        elif graph[i][j] == 2:
+            virus.append((i, j))
+
+def bfs(graph):
+    queue = deque(virus)
     while queue:
         x, y = queue.popleft()
         for i in range(4):
@@ -19,35 +30,23 @@ def bfs(graph, x, y):
                 graph[nx][ny] = 2
                 queue.append((nx, ny))
 
-# 조합을 위한 dfs 함수 정의
-def dfs(graph, count):
-    global result
-    if count == 3:
-        temp = copy.deepcopy(graph)
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    bfs(temp, i, j)
-        count = 0
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 0:
-                    count += 1
-        result = max(result, count)
-        return
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                dfs(graph, count+1)
-                graph[i][j] = 0
+answer = 0
+for i in range(len(walls)):
+    for j in range(i+1, len(walls)):
+        for k in range(j+1, len(walls)):
+            wx1, wy1 = walls[i]
+            wx2, wy2 = walls[j]
+            wx3, wy3 = walls[k]
+            if graph[wx1][wy1] == 0 and graph[wx2][wy2] == 0 and graph[wx3][wy3] == 0:
+                graph[wx1][wy1] = 1
+                graph[wx2][wy2] = 1
+                graph[wx3][wy3] = 1
+                temp = [row[:] for row in graph]
+                bfs(temp)
+                cnt = sum(row.count(0) for row in temp)
+                answer = max(answer, cnt)
+                graph[wx1][wy1] = 0
+                graph[wx2][wy2] = 0
+                graph[wx3][wy3] = 0
 
-# 입력 받기
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-result = 0
-dfs(graph, 0)
-print(result)
+print(answer)
